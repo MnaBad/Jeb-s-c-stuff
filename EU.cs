30b7e57b1a9ca232635b2317b8d42ea962763003
Jeb <oliwiermajdan@gmail.com>
2021-12-10 21:42:55 +0100

    v 1.0

--- EU.cs
+++ EU.cs
@@ -1,13 +1,18 @@
 using System;
+using System.Threading;
 
-namespace MPclass
+namespace C_
 {
     class TextGame
     {
         static private bool[] YesOrNo = { true, false, false, true, false };
-        static private string[] CorrectResponse = { "You win 1", "You win 2", "You win 3", "You win 4", "You win 5" };
+        static public bool[] yesOrNo;
+        static private string[] CorrectResponse = { "You win 1", "You win 2", "You win 3", "You win 4", "You win 5, congrats you did it!" };
+        static public string[] correctResponse;
         static private string[] InncorrectResponse = { "You lose 1", "You lose 2", "You lose 3", "You lose 4", "You lose 5" };
+        static public string[] inncorrectResponse;
         static private string[] Question = { "Question 1", "Question 2", "Question 3", "Question 4", "Question 5" };
+        static public string[] question;
 
         private bool CheckResponse()
         {
@@ -29,6 +34,29 @@ namespace MPclass
 
             return returnValue;
 
+        }
+        public void CheckIfCorrect( string startingMessage, string passed, string failed, bool yOrN )
+        {
+            Console.WriteLine(startingMessage);
+            bool response = CheckResponse();
+            if(response == yOrN)
+            {
+                Console.WriteLine(passed);
+            }
+            else
+            {
+                Console.WriteLine(failed);
+                Thread.Sleep(5000);
+                Console.Clear();
+            }
+        }
+
+        public TextGame()
+        {
+            yesOrNo = YesOrNo;
+            correctResponse = CorrectResponse;
+            inncorrectResponse = InncorrectResponse;
+            question = Question;
         }
     }
 }
